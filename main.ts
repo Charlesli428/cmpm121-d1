9afb20d5e165a517c24f9b32292c0529471e3e02
Charlesli428 <cli310@ucsc.edu>
2025-10-17 10:58:21 +0000

    Step 1 complete: Added button with fire emoji

--- main.ts
+++ main.ts
@@ -1,2 +1,9 @@
 import "./style.css";
 document.body.innerHTML = "";
+
+const app = document.createElement("div");
+document.body.append(app);
+
+const button = document.createElement("button");
+button.textContent = "🔥";
+app.append(button);
